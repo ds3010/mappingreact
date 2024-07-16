458e8231f247a0928053d5e32c17edebf67cab3c
danielseijas3010 <daniel.seijas@cxapp.com>
2024-07-16 10:14:53 -0400

    Small Change

--- src/components/MapRenderer.js
+++ src/components/MapRenderer.js
@@ -7,6 +7,7 @@ import classes from "./MapRenderer.module.css";
 import floorSelector from "../mapFeatures/UiKit/floorSelector";
 import showAssets from "../mapFeatures/AssetKit/ShowAssets";
 import jmapAssetKit from "@jibestream-dev/jmap-asset-kit";
+import ZoneKit from "jmap.js/src/ZoneKit/ZoneKit";
 //import MapUIKit from "@jibestream-dev/jmap-mapui-kit";
 import removeAssets from "../mapFeatures/AssetKit/RemoveAssets";
 
@@ -16,57 +17,61 @@ const MapRenderer = (props) => {
   const [assetConfig, setAssetConfig] = useState(null);
 
   // //INITIALIZING USING JMAP.INIT
-  // useEffect(() => {
-  //   console.log("INITIALIZING MAP");
-  //   const jibestream = jmap.init(props.config);
-
-  //   jmap.dispatcher.subscribe("ready", () => {
-  //     // Scale map out
-  //     jibestream.control.setMapTransform(new jmap.Transform({ scale: 0.3 }));
-  //     const control = jibestream.control;
-  //     const assetKit = new jmapAssetKit(control);
-  //     console.log("CONTROLLER CHANGED, RELOADING!");
-  //     console.log(control);
-  //     setControllerObj(control);
-
-  //     console.log("ASSETKIT CHANGED, RELOADING!");
-  //     setAssetKit(assetKit);
-  //     floorSelector(control);
-  //   });
-  // }, [props.config]);
-
-  //INITIALIZING USING JCORE
   useEffect(() => {
     console.log("INITIALIZING MAP");
-    const { customerId, host, venueId, auth } = props.config;
-    const core = new jmap.core.JCore({ customerId, host, auth });
-    //console.log(core);
-    core.populateVenueByVenueId(venueId, (error, activeVenue) => {
-      if (error) alert(error);
-      // Populate the default Building
-      const buildingId = activeVenue.buildings.getDefault().id;
-      return core.populateBuildingInVenue(activeVenue, buildingId, (error2) => {
-        if (error2) alert(error2);
+    const jibestream = jmap.init(props.config);
 
-        // Make a view controller
-        const control = new jmap.JController({ activeVenue });
-        const assetKit = new jmapAssetKit(control);
+    jmap.dispatcher.subscribe("ready", () => {
+      // Scale map out
+      jibestream.control.setMapTransform(new jmap.Transform({ scale: 0.3 }));
+      const control = jibestream.control;
+      const assetKit = new jmapAssetKit(control);
+      const zoneKit = new ZoneKit(jibestream.core,jibestream.control)
+      console.log(zoneKit)
+      console.log("CONTROLLER CHANGED, RELOADING!");
+      console.log(control);
+      setControllerObj(control);
 
-        console.log("CONTROLLER CHANGED, RELOADING!");
-        setControllerObj(control);
-
-        console.log("ASSETKIT CHANGED, RELOADING!");
-        setAssetKit(assetKit);
-
-        //floorSelector(control);
-
-        // This targets '<div class="map"></div>' in the DOM
-        //const control = new jmap.JController({ activeVenue, width, height });
-        //Show map
-        control.showDefaultMap();
-      });
+      console.log("ASSETKIT CHANGED, RELOADING!");
+      setAssetKit(assetKit);
+      floorSelector(control);
     });
   }, [props.config]);
+
+  //INITIALIZING USING JCORE
+  // useEffect(() => {
+  //   console.log("INITIALIZING MAP");
+  //   const { customerId, host, venueId, auth } = props.config;
+  //   const core = new jmap.core.JCore({ customerId, host, auth });
+  //   //console.log(core);
+  //   core.populateVenueByVenueId(venueId, (error, activeVenue) => {
+  //     if (error) alert(error);
+  //     // Populate the default Building
+  //     const buildingId = activeVenue.buildings.getDefault().id;
+  //     return core.populateBuildingInVenue(activeVenue, buildingId, (error2) => {
+  //       if (error2) alert(error2);
+
+  //       // Make a view controller
+  //       const control = new jmap.JController({ activeVenue });
+  //       const assetKit = new jmapAssetKit(control);
+  //       const zoneKit = new ZoneKit(core,control)
+  //       console.log(zoneKit)
+
+  //       console.log("CONTROLLER CHANGED, RELOADING!");
+  //       setControllerObj(control);
+
+  //       console.log("ASSETKIT CHANGED, RELOADING!");
+  //       setAssetKit(assetKit);
+
+  //       //floorSelector(control);
+
+  //       // This targets '<div class="map"></div>' in the DOM
+  //       //const control = new jmap.JController({ activeVenue, width, height });
+  //       //Show map
+  //       control.showDefaultMap();
+  //     });
+  //   });
+  // }, [props.config]);
 
   const resetAssetHandler = () => {
     showAssets(controllerObj, assetKitObj);
@@ -88,7 +93,7 @@ const MapRenderer = (props) => {
     let config = JSON.parse(localStorage.getItem("assetConfig"));
 
     assetsToFilter.forEach((assetOut) => {
-      config = config.filter((asset) => asset.id.toString() != assetOut);
+      config = config.filter((asset) => asset.id.toString() !== assetOut);
     });
     localStorage.setItem("selectedItems", JSON.stringify([]));
     localStorage.setItem("assetConfig", JSON.stringify(config));
